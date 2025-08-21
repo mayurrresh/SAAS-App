774221fad317f628a5f3c0e020061f7c627dd4ac
mayurrresh <mayureshkahar777@gmail.com>
2025-08-22 01:20:12 +0530

    prepare for railway

--- swaap/settings.py
+++ swaap/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-6i7(s-#c49)^7xmjg@9(t#$7qen=44hgnk=*r_p-f6rftb(%wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas/prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += ["127.0.0.1", 
+                      "localhost",
+    ] 
 
 
 # Application definition
--- swaap/views.py
+++ swaap/views.py
@@ -1,27 +1,41 @@
 import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 from django.http import HttpResponse
 
 from visits.models import PageVisit
 
+LOGIN_URL = settings.LOGIN_URL
+
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        print(request.user.first_name)
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "My Page"
     html_template = "home.html"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": page_qs.count() * 100.0 / qs.count(),
-        "total_visit_count": qs.count()
-        } 
-  
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
     PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)    
+    return render(request, html_template, my_context)
 
-from django.http import HttpResponse
+
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
